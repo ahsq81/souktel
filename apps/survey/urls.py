21f1ba0b7dab3f62f5f6d66cc992fe3201b1c17d
Ahmad said <ahmad@Souktel.(none)>
2010-03-25 16:00:18 +0200

    update survey using template

---
+++ apps/survey/urls.py
@@ -0,0 +1,10 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+from django.conf.urls.defaults import *
+import views
+
+urlpatterns = patterns('',
+    url(r'^survey/?$', views.index),
+    url(r'^survey/(\d+)/?$', views.profile)
+)
--- apps/survey/views.py
+++ apps/survey/views.py
@@ -1,2 +1,22 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+from django.http import HttpResponse
+from rapidsms.webui.utils import render_to_response
+from models import SrProfile
+
+
+def index(request):
+
+    try:
+        SrProfile_opj = SrProfile.objects.all()
+        numberof_M = (float) (SrProfile.objects.filter(sex='M').count())/(SrProfile.objects.count())
+        numberof_F = (float) (SrProfile.objects.filter(sex='F').count())/(SrProfile.objects.count())
+
+    except Exception, e:
+        print e
+        return HttpResponse("Error: %s" % e)
+
+
+    return render_to_response(request,'survey/index.html',{"profiles":SrProfile_opj, "n_male": numberof_M , "n_female":numberof_F})
+
+def profile(request, userid):
+    profile = SrProfile.objects.get(id = userid)
+    return render_to_response(request,'survey/index.html',{"profile":profile})
